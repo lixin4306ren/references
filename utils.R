66ad9620a294147e3647c15b54a9065d6783a3e2
lixin4306ren <lixin4306ren@gmail.com>
2016-08-26 17:45:39 -0400

    s

--- utils.R
+++ utils.R
@@ -1,11 +1,13 @@
 library(htmltools)
 thumbnail <- function(title, img, href, caption = TRUE) {
   div(class = "col-sm-4",
-      a(class = "thumbnail", title = title, href = href,
+      a(
+        class = "thumbnail",
+        title = title,
+        href = href,
         img(src = img),
         div(class = ifelse(caption, "caption", ""),
-          ifelse(caption, title, "")
-        )
-      )
-  )
+            ifelse(caption, title, ""))
+      ))
+  
 }
